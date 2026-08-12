577da85e1598ad049b6e6add70419c418322f10d
Conor Lyons <conorlyons36@gmail.com>
2026-08-12 16:59:28 +0100

    Add a view all/single deck(s) pages

--- myapp/urls.py
+++ myapp/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("decks", views.view_all_decks, name="all-decks"),
+    path("decks/<int:deckid>", views.view_single_deck, name="single-deck"),
+    
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,6 +1,16 @@
 from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404, redirect
 from django.shortcuts import render
+from .models import *
 
 # Create your views here.
 def index(request):
     return render(request, "index.html")
+
+def view_all_decks(request):
+    all_decks = Deck.objects.all()
+    return render(request, "all-decks.html", {"decks": all_decks})
+
+def view_single_deck(request, deckid):
+    single_deck = get_object_or_404(Deck, id=deckid)
+    return render(request, "single-deck.html", {"deck":single_deck})
